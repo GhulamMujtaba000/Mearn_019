94fab51f274edec7b986869748e11c8e650de1d2
Ghulam Mujtaba Azhar <36321271+GhulamMujtaba000@users.noreply.github.com>
2023-11-01 22:32:38 +0500

    updated project

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (<React.Fragment>
+    <div className="container">
+      <div className="item">
+        <h1>Hello React </h1>
+        </div>
+        <div className="item">
+        <h1>Hello React </h1>
+        </div>
+        <div className="item">
+        <h1>Hello React </h1>
+        </div>
+        <div className="item">
+        <h1>Hello React </h1>
+        </div>
+        <div className="item">
+        <h1>Hello React </h1>
+        </div> <div className="item">
+        <h1>Hello React </h1>
+        </div> <div className="item">
+        <h1>Hello React </h1>
+        </div> <div className="item">
+        <h1>Hello React </h1>
+        </div> <div className="item">
+        <h1>Hello React </h1>
+        </div>
+
+      
+      
+    </div>
+    <div className="buttons">
+      <button className="clickme">
+        Clic Me 
+        
+      </button>
+
+      
+    </div>
+   
+  </React.Fragment>)
+}
+
+export default App
